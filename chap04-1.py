52f4791e24d87b8cd1269ff83c33a2d0ba5e8b2a
sejin.eom <sejin1031@naver.com>
2020-01-05 15:55:14 +0900

    chap04 finish\

--- chap04-1.py
+++ chap04-1.py
@@ -22,10 +22,11 @@ x_train, x_test , y_train, y_test = train_test_split(x,y,stratify=y,test_size=0.
 
 # print(np.unique(y_train,return_counts=True))
 
-class LogisticNeuron:
+class SingleLayer:
     def __init__(self):
         self.w = None
         self.b = None
+        self.losses = []
     
     def forpass(self,x):
         z = np.sum(x*self.w) + self.b
@@ -40,13 +41,19 @@ class LogisticNeuron:
         self.w = np.ones(x.shape[1])
         self.b = 0
         for i in range(epochs):
-            for x_i, y_i in zip(x,y):
-                z = self.forpass(x_i)
+            loss = 0
+            indexes = np.random.permutation(np.arange(len(x)))
+            for i in indexes:
+                z = self.forpass(x[i])
                 a = self.activation(z)
-                err = -(y_i-a)
-                w_grad , b_grad = self.backprop(x_i,err)
+                err = -(y[i]-a)
+                w_grad , b_grad = self.backprop(x[i],err)
                 self.w -= w_grad
                 self.b -= b_grad
+                a = np.clip(a,1e-10,1-1e-10)
+                loss += -(y[i]*np.log(a)+(1-y[i])*np.log(1-a))
+            self.losses.append(loss/len(y))
+            
     
     def activation(self,z):
         a = 1 / (1 + np.exp(-z))
@@ -56,8 +63,19 @@ class LogisticNeuron:
         z = [self.forpass(x_i) for x_i in x]
         a = self.activation(np.array(z))
         return a > 0.5
+    
+    def predict(self,x):
+        z = [self.forpass(x_i) for x_i in x]
+        return np.array(z) > 0
+    
+    def score(self,x,y):
+        return np.mean(self.predict(x)==y)
 
-neuron = LogisticNeuron()
-neuron.fit(x_train,y_train)
+layer = SingleLayer()
+layer.fit(x_train,y_train)
 
-print(np.mean(neuron.predict(x_test) == y_test))
+
+print(layer.score(x_test,y_test))
+
+plt.plot(layer.losses)
+plt.show()
